0e19483b32a3e55242ed4b69107e5148fb8ab884
goporolo0 <81228006+goporolo0@users.noreply.github.com>
2021-07-19 11:53:50 +0700

    Update Server.py

--- Server.py
+++ Server.py
@@ -1,10 +1,12 @@
 import socket
 import json
-from tkinter.constants import SE
 import requests
 import threading
 import os
 import time
+import tkinter as tk
+from tkinter import Canvas, filedialog, Text
+clients = []
 
 
 def getInfo():
@@ -29,6 +31,77 @@ def Register(username, password):
         print('client left')
     finally:
         file.close()
+
+
+def GuiFileDataCacBankChoClientKhiTracuu(ClientConnection, NgayThangData):
+    datavcb = GetsizeData("./17072021/vcb.json")
+    lenvcb = DodaiChuoiso(datavcb)
+    ClientConnection.send(lenvcb.encode())
+    ClientConnection.send(datavcb.encode())
+
+    datactg = GetsizeData("./"+NgayThangData+"/ctg.json")
+    lenctg = DodaiChuoiso(datactg)
+    ClientConnection.send(lenctg.encode())
+    ClientConnection.send(datactg.encode())
+
+    datatcb = GetsizeData("./"+NgayThangData+"/tcb.json")
+    lentcb = DodaiChuoiso(datatcb)
+    ClientConnection.send(lentcb.encode())
+    ClientConnection.send(datatcb.encode())
+
+    databid = GetsizeData("./"+NgayThangData+"/bid.json")
+    lenbid = DodaiChuoiso(databid)
+    ClientConnection.send(lenbid.encode())
+    ClientConnection.send(databid.encode())
+
+    datastb = GetsizeData("./"+NgayThangData+"/stb.json")
+    lenstb = DodaiChuoiso(datastb)
+    ClientConnection.send(lenstb.encode())
+    ClientConnection.send(datastb.encode())
+
+    SendFileExchangeToClient(ClientConnection, "./" +
+                             NgayThangData+"/vcb.json", int(datavcb))
+    SendFileExchangeToClient(ClientConnection, "./" +
+                             NgayThangData+"/ctg.json", int(datactg))
+    SendFileExchangeToClient(ClientConnection, "./" +
+                             NgayThangData+"/tcb.json", int(datatcb))
+    SendFileExchangeToClient(ClientConnection, "./" +
+                             NgayThangData+"/bid.json", int(databid))
+    SendFileExchangeToClient(ClientConnection, "./" +
+                             NgayThangData+"/stb.json", int(datastb))
+
+
+def GuiFileDataNgayMoiChoClientHomNay(ClientConnection):
+    datavcb = GetsizeData("vcb.json")
+    lenvcb = DodaiChuoiso(datavcb)
+    ClientConnection.send(lenvcb.encode())
+    ClientConnection.send(datavcb.encode())
+
+    datactg = GetsizeData("ctg.json")
+    lenctg = DodaiChuoiso(datactg)
+    ClientConnection.send(lenctg.encode())
+    ClientConnection.send(datactg.encode())
+
+    datatcb = GetsizeData("tcb.json")
+    lentcb = DodaiChuoiso(datatcb)
+    ClientConnection.send(lentcb.encode())
+    ClientConnection.send(datatcb.encode())
+
+    databid = GetsizeData("bid.json")
+    lenbid = DodaiChuoiso(databid)
+    ClientConnection.send(lenbid.encode())
+    ClientConnection.send(databid.encode())
+
+    datastb = GetsizeData("stb.json")
+    lenstb = DodaiChuoiso(datastb)
+    ClientConnection.send(lenstb.encode())
+    ClientConnection.send(datastb.encode())
+
+    SendFileExchangeToClient(ClientConnection, "vcb.json", int(datavcb))
+    SendFileExchangeToClient(ClientConnection, "ctg.json", int(datactg))
+    SendFileExchangeToClient(ClientConnection, "tcb.json", int(datatcb))
+    SendFileExchangeToClient(ClientConnection, "bid.json", int(databid))
+    SendFileExchangeToClient(ClientConnection, "stb.json", int(datastb))
 
 
 def XulyNhuCauClient(ClientConnection):
@@ -83,6 +156,33 @@ def XulyNhuCauClient(ClientConnection):
                     ClientConnection, "stb.json", int(datastb))
                 SendFileExchangeToClient(
                     ClientConnection, "sbv.json", int(datasbv))
+
+                while True:
+                    # Lỗi ở đây bởi vì mình chỉ bấm Tra cứu có 1 lần
+                    CheckTracuu = ClientConnection.recv(1024).decode()
+                    if(CheckTracuu == "Tracuu"):
+                        while True:
+                            KiemTraDataSeGui = ClientConnection.recv(
+                                1024).decode()
+                            if(KiemTraDataSeGui == "17072021"):
+                                GuiFileDataCacBankChoClientKhiTracuu(
+                                    ClientConnection, "17072021")
+                            elif(KiemTraDataSeGui == "18072021"):
+                                GuiFileDataCacBankChoClientKhiTracuu(
+                                    ClientConnection, "18072021")
+                            elif(KiemTraDataSeGui == "19072021"):
+                                GuiFileDataCacBankChoClientKhiTracuu(
+                                    ClientConnection, "19072021")
+                            elif(KiemTraDataSeGui == "Today"):
+                                GuiFileDataNgayMoiChoClientHomNay(
+                                    ClientConnection)
+                            elif(KiemTraDataSeGui == "Thoat"):
+                                break
+                        continue
+                    elif(CheckTracuu == 'Thoat'):
+                        break
+                    else:
+                        break
             else:
                 ClientConnection.send(
                     "Ban nhap sai tai khoan hoac mat khau".encode())
@@ -152,7 +252,6 @@ def SendFileExchangeToClient(SOCKET, filenamejson, datasize):
     file = open(filenamejson, 'rb')
     file_data = file.read(datasize)
     SOCKET.send(file_data)
-    print("Data has been transmitted successfully")
     file.close()
 
 
@@ -174,6 +273,7 @@ def Docfile(username, password):
 
 
 def worker(ClientConnection):
+
     XulyNhuCauClient(ClientConnection)
 
     while True:
@@ -207,7 +307,13 @@ def updateEX():
             start = time.time()
 
 
+def kill_clients(clients):
+    for client in clients:
+        client.close()
+
+
 def run_app():
+
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     soc.bind((HOST_ADDRESS, PORT))
     soc.listen()
@@ -218,12 +324,23 @@ def run_app():
             print("Connect to Client: ", ClientAddress)
             content = "Server say hello Client"
             ClientConnection.send(content.encode())
+            clients.append(ClientConnection)
 
             thread = threading.Thread(target=worker, args=(ClientConnection,))
             thread.start()
 
         except Exception as e:
             print('client left')
+
+
+def MENU():
+    root = tk.Tk()
+    Canvas = tk.Canvas(root, height=400, width=400, bg='RoyalBlue4')
+    Canvas.pack()
+    korosai = tk.Button(root, text="CLOSE CONNECTIONS", font='Times 15',
+                        bg="RoyalBlue4", fg="white", command=lambda: kill_clients(clients))
+    korosai.pack()
+    root.mainloop()
 
 
 if(__name__ == "__main__"):
@@ -235,3 +352,5 @@ if(__name__ == "__main__"):
     threadApp.start()
     threadTime = threading.Thread(target=updateEX)
     threadTime.start()
+    threadMenu = threading.Thread(target=MENU)
+    threadMenu.start()
